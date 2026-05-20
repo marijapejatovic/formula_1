8fd8f25f8336b394f287e9091ef4fabec2af179f
marijapejatovic <pejatovicmarija28@gmail.com>
2026-05-20 15:32:20 +0200

    add validation

--- bronze_silver_gold.py
+++ bronze_silver_gold.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine,text 
 from dotenv import load_dotenv
 import os
 class DataCleaner:
@@ -13,19 +13,15 @@ class DataCleaner:
         return self
     def fix_dates_columns(self, columns:list)->"DataCleaner":
         for col in columns:
-            self.df[col]=pd.to_datetime(self.df[col],format="mixed", errors="coerce")
+            self.df[col]=pd.to_datetime(self.df[col],format="mixed", errors="coerce") 
         return self
     def fix_time(self, columns:list)->"DataCleaner":
         for col in columns:
-            self.df[col]=pd.to_datetime(self.df[col], format="%I:%M:%S %p", errors="coerce")
+            self.df[col]=pd.to_datetime(self.df[col], format="%I:%M:%S %p", errors="coerce").dt.strftime("%H:%M:%S")
         return self
     def fix_numerical(self, columns:list)->"DataCleaner":
         for col in columns:
             self.df[col]=pd.to_numeric(self.df[col], errors="coerce")
-        return self
-    def standardize_intervals(self, columns:list)->"DataCleaner":
-        for col in columns:
-            self.df[col]=pd.to_timedelta(self.df[col], errors="coerce")
         return self
     def standardize_lowercase(self, columns:list)->"DataCleaner":
         for col in columns:
@@ -47,16 +43,46 @@ if __name__=="__main__":
     bronze_row.to_sql("bronze_row", engine, if_exists="replace", index=False)
     bronze_from_db = pd.read_sql("SELECT * FROM bronze_row", engine)
     silver = DataCleaner(bronze_from_db, "silver_row")
-    silver_row=(silver.standardize_text_columns(["positionText", "name_x", "name_y", "location", "country", "forename", "surname", "nationality", "nationality_constructors", "positionText_constructorstandings", "status"])
+    silver_row=(silver.standardize_text_columns(["positionText", "name_x", "name_y", "location", "country", "forename", "surname", "nationality", "nationality_constructors", "positionText_constructorstandings", "status", "duration"])
                                   .fix_dates_columns(["date", "quali_date", "dob", "sprint_date"])
                                   .fix_time(["time", "fastestLapTime","time_races","quali_time", "sprint_time", "time_laptimes", "time_pitstops"])
                                   .fix_numerical(["resultId", "raceId", "driverId", "constructorId", "number", "grid", "position", "positionOrder", "points", "laps", "milliseconds","fastestLap", "rank", "fastestLapSpeed", "statusId", "year", "round","lat", "lng", "alt", "number_drivers", "lap", "position_laptimes", "milliseconds_laptimes", "lap_pitstops", "milliseconds_pitstops", "stop","driverStandingsId", "points_driverstandings", "position_driverstandings", "wins", "constructorStandingsId", "points_constructorstandings", "position_constructorstandings", "wins_constructorstandings"])
-                                  .standardize_intervals(["duration"])
                                   .standardize_url(["url_x", "url_y", "url", "url_constructors"])
                                   .standardize_lowercase(["circuitRef", "driverRef", "constructorRef"])
                                   .standardize_uppercase(["code"])
     )
     silver_row.df.to_csv("silver.csv", index=False)
+    silver_row.df.to_sql("silver_row", engine, if_exists="replace", index=False)
+    pd.set_option("display.max_columns", None)
+    pd.set_option("display.max_rows", None)
+    print(f"Bronze redovi {len(bronze_row)}")
+    print(f"Silver redovi {len(silver_row.df)}")
+    print(f"izgubljeni redovi {len(bronze_row)-len(silver_row.df)}")
+    print("Null vrijednosti po kolonama:")
+    print(silver_row.df.isnull().sum())
+    print("tipovi podataka po kolonama:")
+    print(silver_row.df.dtypes)
+   #with engine.connect() as conn:
+        #ith open("tabela.sql", "r") as f:
+           #conn.execute(text(f.read()))
+       #conn.commit() 
+   #dim_status=pd.read_sql("select distinct statusId, status from silver_row", engine)
+   #dim_status.to_sql("dim_status", engine, if_exists="append", index=False)
+   #dim_constructors=pd.read_sql("select distinct constructorId, constructorRef, name, nationality_constructor, url_constructors from silver_row", engine)
+   #dim_constructors.to_sql("dim_constructors", engine, if_exists="append", index=False)
+   #dim_driver=pd.read_sql("select distinct driverId, number, driverRef, number_drivers, code, forename, surname, dob, nationality, url from silver_row", engine)
+   #dim_driver.to_sql("dim_driver", engine, if_exists="append", index=False)
+   #dim_race=pd.read_sql("select distinct raceId, year, round, name_x, url_x, quali_date, quali_time, date, time_races, sprint_date, sprint_time from silver_row", engine)
+   #dim_race.to_sql("dim_race", engine, if_exists="append", index="False")
+
+
+
+    
+                           
+
+
+
+
     
 
    
